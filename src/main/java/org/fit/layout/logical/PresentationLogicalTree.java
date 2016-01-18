995b8fc3320e9d5607e106981c446d1fc29bfc8c
Radek Burget <burgetr@fit.vutbr.cz>
2016-01-18 13:44:08 +0100

    Use the default LogicalAreaTree implementation

--- src/main/java/org/fit/layout/logical/PresentationLogicalTree.java
+++ src/main/java/org/fit/layout/logical/PresentationLogicalTree.java
@@ -5,38 +5,19 @@
  */
 package org.fit.layout.logical;
 
+import org.fit.layout.impl.DefaultLogicalAreaTree;
 import org.fit.layout.model.AreaTree;
-import org.fit.layout.model.LogicalArea;
-import org.fit.layout.model.LogicalAreaTree;
 
 /**
  * 
  * @author burgetr
  */
-public class PresentationLogicalTree implements LogicalAreaTree
+public class PresentationLogicalTree extends DefaultLogicalAreaTree
 {
-    private AreaTree atree;
-    private LogicalArea root;
     
     public PresentationLogicalTree(AreaTree atree)
     {
-        this.atree = atree;
+        super(atree);
     }
     
-    public AreaTree getAreaTree()
-    {
-        return atree;
-    }
-
-    @Override
-    public LogicalArea getRoot()
-    {
-        return root;
-    }
-
-    public void setRoot(LogicalArea root)
-    {
-        this.root = root;
-    }
-
 }
